efad84da456ddf6ecd754ece06fb5b636324e964
Nik Kennedy <github@hzdev.net>
2017-08-05 14:20:32 +0100

    added helloworld example skill

---
+++ skills/helloworld/index.js
@@ -0,0 +1,25 @@
+// straight from http:/github.com/alexa/skill-sample-nodejs-hello-world Amazon example
+// should work out of the box - follow amazon guide for setting up this example
+// but use https://<yourdomainname>/alexa/helloworld as the https endpoint
+
+
+'use strict';
+var Alexa = require("alexa-sdk");
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('SayHello');
+    },
+    'HelloWorldIntent': function () {
+        this.emit('SayHello')
+    },
+    'SayHello': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
